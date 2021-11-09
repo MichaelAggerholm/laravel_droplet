712be33742ae536ced5d2c40a0884ba35f34655f
Michael Aggerholm <92aggerholm@gmail.com>
2021-11-09 21:50:11 +0100

    created importer for csv files to database with laral-excel library.

---
+++ app/Imports/BooksImport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Book;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class BooksImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+//        dd($row);
+
+        return new Book([
+            'supplierId'    => 'booklist' . $row['id'],
+            'title'         => $row['title'],
+            'author'        => $row['author'],
+            'format'        => $row['format'],
+            'price'         => $row['price'],
+        ]);
+
+        dd($row);
+    }
+}
---
+++ database/migrations/2021_11_09_201031_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('supplierId')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->string('format');
+            $table->float('price');
+            $table->integer('isDeleted')->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
